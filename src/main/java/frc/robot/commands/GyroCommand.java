e34cfd5e9011cbb334064e44513967b63d3e02f1
Scoresh <64853172+Scoresh@users.noreply.github.com>
2024-01-29 19:50:06 -0500

    Commented GyroCommand.java

--- src/main/java/frc/robot/commands/GyroCommand.java
+++ src/main/java/frc/robot/commands/GyroCommand.java
@@ -6,7 +6,7 @@ import frc.robot.subsystems.Gyroscope;
 
 
 public class GyroCommand extends Command{
-    
+    //!!IMPORTANT!! THIS CODE HAS NOT BEEN TOUCHED UPON. GYROSCOPES ARE VERY TRICKY AND HARD TO WORK WITH SO IF U WANT TO ADD ON, good luck. 
     XboxController m_box;
     Gyroscope m_gyro;
     public GyroCommand(XboxController xbox, Gyroscope gyro){
