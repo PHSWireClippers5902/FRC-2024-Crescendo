dd4272f6a473af488e6eab32f6d0aea7e6546e84
Scoresh <64853172+Scoresh@users.noreply.github.com>
2024-01-29 19:35:24 -0500

    Commented DeepSpaceCommand.java

--- src/main/java/frc/robot/commands/DeepSpaceCommand.java
+++ src/main/java/frc/robot/commands/DeepSpaceCommand.java
@@ -7,11 +7,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DeepSpace;
 
 public class DeepSpaceCommand extends Command{
+    //!!IMPORTANT!! THIS COMMAND ONLY WORKS ON 2019 DUCKBOT. TO USE THIS, COMMENT MOST OF THE STUFF IN THE ROBOTCONTAINER OUT, AND UNCOMMENT THE INITIALIZATION OF THE DEEPSPACE COMMAND AND SUBSYSTEM
+    //initializes the subsystem space System, and the xbox controller 
     XboxController controller;
     DeepSpace spaceSystem;
 
 
     public DeepSpaceCommand (XboxController xbox, DeepSpace space){
+        //sets the values, and addrequirements of the subsystem. This code is essential. 
         spaceSystem = space;
         controller = xbox;
         //addRequirement(spaceSystem);
@@ -19,8 +22,10 @@ public class DeepSpaceCommand extends Command{
     }
     @Override
     public void execute() {
+        //moves the wheels by the xbox controllers joysticks. 
         spaceSystem.moveLeft(-0.3*controller.getLeftY());
         spaceSystem.moveRight(-0.3*controller.getRightY());
+        //if the bumper is pressed, then move the intake thingy. 
         if (controller.getLeftBumper()){
             spaceSystem.moveIntake(1);
 
